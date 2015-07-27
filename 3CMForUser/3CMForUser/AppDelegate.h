469ad1d74c7e73dd840f824c0723ae30cf573368
ANine <apple@ANines-MacBook-Pro.local>
2015-07-27 17:07:00 +0800

    *.创建一个新项目

---
+++ 3CMForUser/3CMForUser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3CMForUser
+//
+//  Created by ANine on 7/27/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
